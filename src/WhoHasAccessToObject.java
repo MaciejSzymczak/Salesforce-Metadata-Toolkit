ee0824505be97a94248836fe3554b3055a4885c1
MaciejSzymczak <soft@home.pl>
2023-05-04 07:00:16 +0200

    Update WhoHasAccessToObject.java

--- src/WhoHasAccessToObject.java
+++ src/WhoHasAccessToObject.java
@@ -1,11 +1,20 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -18,18 +27,13 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+
 /**
- * This java program  produces a matrix that shows what fields are used on what page layouts (the matrix: fields x page layouts).
- * It reads the content of page layouts and the definition of the object (you can download the .layout and .object files using migration tool) and conglomerates data in a form of a readable matrix. 
- * It Reads given folder on the disk (entry parameter of java program) and saves the result in the file result.csv.
- * It analyzes page layouts, not visual force pages (so far).
- * It can be used for identification what region uses what fields and for identification what fields are not used at all.
- * Column C says the number of page layouts on which field is used. 0 mean the field is suspicious and should be prosecuted  rather than instant death sentence. 
  * 
  * @author Maciej Szymczak
- * @version 2016.11.23
- * @param  Folder name with <pageLayuouts>.layout and <objectdefinition>.object
- * @return result.csv
+ * @version 2023.05.04
+ * @param  Folder with profiles and object name
+ * @return result.csv and result_you_can_open_me_in_Excel.xml
  */
 
 public class WhoHasAccessToObject {
@@ -38,9 +42,9 @@ public class WhoHasAccessToObject {
 	static Map rows = new TreeMap<String, String>();
 		
     public static void 	writeToCsv(String folderName) throws IOException {
-		  FileWriter fw = new FileWriter(folderName+"\\result.csv");
-		  
 		  System.out.println("Creating file: result.csv");
+
+		  FileWriter fw = new FileWriter(folderName+"\\result.csv");
 		  
 		  //file header
 	      fw.write( "fileName,allowRead,allowCreate,allowDelete,allowEdit,viewAllRecords,modifyAllRecords\n" );
@@ -52,14 +56,129 @@ public class WhoHasAccessToObject {
 	      
 		  fw.close();
     }
+
+    public static Node createRow(Document document, String pfileName, String commaSeparatedValues) {
+
+	    StringTokenizer st1 = new StringTokenizer(commaSeparatedValues, ",");
+	    String pallowRead = st1.nextToken();
+	    String pallowCreate = st1.nextToken();
+	    String pallowDelete = st1.nextToken();
+	    String pallowEdit = st1.nextToken();
+	    String pviewAllRecords = st1.nextToken();
+	    String pmodifyAllRecords = st1.nextToken();
+		       	
+	    // create FirstName and LastName elements
+	    Element fileName = document.createElement("fileName");
+	    Element allowRead = document.createElement("allowRead");
+	    Element allowCreate = document.createElement("allowCreate");
+	    Element allowDelete = document.createElement("allowDelete");
+	    Element allowEdit = document.createElement("allowEdit");
+	    Element viewAllRecords = document.createElement("viewAllRecords");
+	    Element modifyAllRecords = document.createElement("modifyAllRecords");
+
+	    fileName.appendChild(document.createTextNode(pfileName));
+	    allowRead.appendChild(document.createTextNode(pallowRead));
+	    allowCreate.appendChild(document.createTextNode(pallowCreate));
+	    allowDelete.appendChild(document.createTextNode(pallowDelete));
+	    allowEdit.appendChild(document.createTextNode(pallowEdit));
+	    viewAllRecords.appendChild(document.createTextNode(pviewAllRecords));
+	    modifyAllRecords.appendChild(document.createTextNode(pmodifyAllRecords));
+
+	    // create contact element
+	    Element row = document.createElement("row");
+
+	    row.appendChild(fileName);
+	    row.appendChild(allowRead);
+	    row.appendChild(allowCreate);
+	    row.appendChild(allowDelete);
+	    row.appendChild(allowEdit);
+	    row.appendChild(viewAllRecords);
+	    row.appendChild(modifyAllRecords);
+
+	    return row;
+	  }	
+	
+	private static void writeToXML(String folderName) throws SAXException, IOException, ParserConfigurationException {
+    //source: http://www.java2s.com/Code/Java/XML/CreateselementnodeattributenodecommentnodeprocessinginstructionandaCDATAsection.htm
+		
+		/*
+			<root>
+			    <row>
+			        <members>Historical_PRN_Credit_Memo__c</members>
+			        <allowRead>CustomObject</allowRead>
+			        <allowCreate>CustomObject</allowCreate>
+			    </row>
+			    ...
+			    <row>
+			        <members>*</members>
+			        <allowRead>Profile</allowRead>
+			        <allowCreate>CustomObject</allowCreate>
+			    </row>
+			</root>
+		 */
+		
+		System.out.println("Creating file: result_you_can_open_me_in_Excel.xml");
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document document;
+		
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		document = builder.newDocument();
+		  
+		Element root = document.createElement("root");
+		document.appendChild(root);		
+	    
+		for(Object row: rows.keySet()) {
+			  String commaSeparatedValues = (String)rows.get(row);
+			  String pfileName = (String) row;
+			  Node nodeRow = createRow(document, pfileName, commaSeparatedValues);
+			  root.appendChild(nodeRow);		
+		}		      
+	    
+		String fileName = folderName+"\\result_you_can_open_me_in_Excel.xml";
+		saveChanges(document, fileName);				
+	}
+	
+	private static void saveChanges(Document doc, String fileName) 
+	{
+		try {
+			Transformer tr = TransformerFactory.newInstance().newTransformer();
+			tr.setOutputProperty(OutputKeys.INDENT, "yes");
+			tr.setOutputProperty(OutputKeys.METHOD, "xml");
+			tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			//tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+			tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	
+			// send DOM to file
+			tr.transform(new DOMSource(doc), 
+			new StreamResult(new FileOutputStream(fileName)));
+	
+		} catch (TransformerException te) {
+			System.out.println(te.getMessage());
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+	}	
 	
 	/**
 	* analyseXMLLayout
 	 * @throws XPathExpressionException 
 	*/
-	public static void analyseXML (String path, String fileName, String sObjectName) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
-		//rows.put(fileName, "");
+	public static void analyseXML (String path, String fileName, String sObjectName) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {		
+		/* analysed nodes:
+		    <objectPermissions>
+		        <allowCreate>false</allowCreate>
+		        <allowDelete>false</allowDelete>
+		        <allowEdit>false</allowEdit>
+		        <allowRead>true</allowRead>
+		        <modifyAllRecords>false</modifyAllRecords>
+		        <object>Historical_PRN_Credit_Memo__c</object>
+		        <viewAllRecords>true</viewAllRecords>
+		    </objectPermissions>    
+		 */		
 		
+		//to add all files to file
+		//rows.put(fileName, "");
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setValidating(false);
 		factory.setExpandEntityReferences(false);
@@ -127,13 +246,14 @@ public class WhoHasAccessToObject {
 		if (args.length!=2) {
 			System.out.println("@author Maciej Szymczak");
 			System.out.println("**** usage: java -Dfile.encoding=UTF8 -jar jarname.jar <source folder> <objectname>");
-			System.out.println("**** example: java -Dfile.encoding=UTF8 -jar jarname.jar C:\\salesforce_ant_44.0\\work\\adHocDownload\\profiles Historical_PRN_Credit_Memo__c");
+			System.out.println("**** example: java -Dfile.encoding=UTF8 -jar jarname.jar C:\\salesforce_ant_44.0\\work\\adHocDownload Historical_PRN_Credit_Memo__c");
 			System.exit(-1);
 		}
 		String folderName = args[0];
 		String sObjectName = args[1];
 		readFilesFromFolder(new File(folderName), sObjectName);
 		writeToCsv(folderName);
+		writeToXML(folderName);
 		System.out.println("Done!");
 	}
 
